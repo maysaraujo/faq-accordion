efdf5c26e4d343afe74495764124c4693b926764
maysaraujo <maysaaraujo25@gmail.com>
2024-01-20 16:05:41 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import IconStar from './assets/images/icon-star.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='title'>
+        <img src={IconStar} alt='Icon star' />
+        <h1>FAQs</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
